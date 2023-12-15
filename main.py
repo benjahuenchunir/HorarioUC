64126e9930118a9a61199cc03970d2810eab7cf5
TheDarkLeyends <benjastange@gmail.com>
2023-12-15 18:15:33 -0300

    Removed clutter

--- main.py
+++ main.py
@@ -1,55 +1,54 @@
-from backend import Logic
-from frontend import ScheduleWindow, OFGWindow
-import parametros as p
+from backend.logic import Logic
+from frontend.main_window import ScheduleWindow
+from frontend.ofg_window import OFGWindow
 import sys
 from PyQt5.QtWidgets import QApplication
 
-
-class HorarioMaker:
-    def __init__(self):
-        self.front = ScheduleWindow()
-        self.ofgs = OFGWindow()
-        self.back = Logic()
-        self.conectar_senales()
-
-    def conectar_senales(self):
-        self.front.senal_cambiar_campus.connect(self.back.change_campus)
-        self.front.senal_cambiar_creditos.connect(self.back.change_credits)
-        self.front.senal_buscar_sigla.connect(self.back.find_course_info)
-        self.back.senal_add_course.connect(self.front.add_course)
-        self.back.senal_update_schedule.connect(self.front.new_schedule)
-        self.front.senal_borrar_curso.connect(self.back.delete_course)
-        self.front.senal_cambiar_seccion.connect(self.back.filter_course_section)
-        self.back.senal_borrar_curso.connect(self.front.delete_course)
-
-        self.front.senal_buscar_ofgs.connect(self.change_to_ofgs)
-        self.ofgs.senal_cambiar_area.connect(self.back.change_ofg_area)
-        self.back.senal_update_ofgs.connect(self.ofgs.update_ofgs)
-        self.ofgs.senal_volver.connect(self.change_to_main)
-
-    def iniciar(self):
-        self.front.show()
-        """self.front.senal_buscar_sigla.emit("FIS1533")
-        self.front.senal_buscar_sigla.emit("EYP1113")
-        self.front.senal_buscar_sigla.emit("IIC2413")
-        self.front.senal_buscar_sigla.emit("EAG170A")
-        self.front.senal_buscar_sigla.emit("BIO143M")
-        self.front.senal_buscar_sigla.emit("FIS0153")"""
-
-    def change_to_ofgs(self, selected_combination):
-        self.front.hide()
-        self.back.current_combination.clear()
-        for course in selected_combination:
-            self.back.current_combination.append([course])
-        self.ofgs.iniciar()
-
-    def change_to_main(self):
-        self.ofgs.close()
-        self.front.show()
-
+class HorarioUC:
+    def __init__(self) -> None:
+        super().__init__()
+        self.backend = Logic()
+        self.schedule_window = ScheduleWindow()
+        self.ofg_window = OFGWindow()
+        self.conectar_senales_schedule()
+        self.conectar_senales_ofg()
+        self.backend.retrieve_all_courses()
+        self.schedule_window.show()
+    
+    def conectar_senales_schedule(self):
+        self.backend.senal_enviar_cursos.connect(self.schedule_window.add_suggestions)
+        self.schedule_window.senal_buscar_sigla.connect(self.backend.retrieve_course)
+        self.backend.senal_add_course.connect(self.schedule_window.add_course)
+        self.schedule_window.senal_cambiar_seccion.connect(self.backend.filter_course_section)
+        self.schedule_window.senal_borrar_curso.connect(self.backend.delete_course)
+        self.schedule_window.senal_borrar_curso.connect(self.schedule_window.delete_course)
+        self.backend.senal_new_schedule.connect(self.schedule_window.new_schedule)
+        self.backend.senal_update_schedule.connect(self.schedule_window.update_schedule)
+        self.schedule_window.btn_next.clicked.connect(self.backend.increase_index)
+        self.backend.senal_change_next_btn_state.connect(lambda x: self.schedule_window.btn_next.setEnabled(x))
+        self.schedule_window.btn_previous.clicked.connect(self.backend.decrease_index)
+        self.backend.senal_change_prev_btn_state.connect(lambda x: self.schedule_window.btn_previous.setEnabled(x))
+        self.backend.senal_update_index.connect(self.schedule_window.update_current_index_label)
+        self.schedule_window.senal_buscar_ofgs.connect(self.change_to_ofgs)
+    
+    def conectar_senales_ofg(self):
+        self.ofg_window.senal_cambiar_area.connect(self.backend.retrieve_ofg_area)
+        self.ofg_window.btn_back.clicked.connect(lambda: self.ofg_window.hide() or self.schedule_window.show())
+        self.ofg_window.btn_next.clicked.connect(self.backend.increase_ofg_index)
+        self.backend.senal_change_next_btn_state_ofg.connect(lambda x: self.ofg_window.btn_next.setEnabled(x))
+        self.ofg_window.btn_previous.clicked.connect(self.backend.decrease_ofg_index)
+        self.backend.senal_change_prev_btn_state_ofg.connect(lambda x: self.ofg_window.btn_previous.setEnabled(x))
+        self.backend.senal_update_index_ofg.connect(self.ofg_window.update_current_index_label)
+        self.backend.senal_new_schedule_ofg.connect(self.ofg_window.new_schedule)
+        self.backend.senal_update_schedule_ofg.connect(self.ofg_window.update_schedule)
+    
+    def change_to_ofgs(self):
+        self.schedule_window.hide()
+        self.ofg_window.iniciar()
+        self.ofg_window.new_schedule(self.backend.combinaciones[self.backend.current_course_index], 0, 0)
+        
 
 if __name__ == "__main__":
-
     def hook(type_, value, traceback):
         print(type_)
         print(traceback)
@@ -57,7 +56,9 @@ if __name__ == "__main__":
     sys.__excepthook__ = hook
 
     app = QApplication(sys.argv)
-    main = HorarioMaker()
-    main.iniciar()
-
+    
+    horario = HorarioUC()
+    horario.backend.retrieve_course("IIC1005")
+    horario.backend.retrieve_course("MAT1630")
+    
     sys.exit(app.exec())
